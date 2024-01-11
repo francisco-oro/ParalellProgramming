266fa7ed504764eca4bd96face9dc07afbd1051a
francisco-oro <francisco.oro@comunidad.unam.mx>
2024-01-11 09:25:49 -0600

    Test-Driven-Development

---
+++ src/UnitTesting/Web/Controllers/CalculateController.cs
@@ -0,0 +1,18 @@
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculateController : ControllerBase
+    {
+
+        [HttpGet(Name = "Add/{left}/{right}")]
+        public int Get(int left, int right)
+        {
+            return new Calculator().Sum(left, right);
+        }
+        
+    }
+}
